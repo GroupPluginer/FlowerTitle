84a270fc9e5b61ae21cbeb0b01bdc4cbc05047f8
GroupPluginer <4gamesl7o@gmail.com>
2017-08-04 15:29:33 +0300

    Create Main.php

---
+++ src/title/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace title;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+use pocketmine\utils\TextFormat as TF;
+
+ public function onEnable(){
+  $this->getServer()->getPluginManager()->registerEvent($this, $this);
+  $this->getServer()->getLogger()->info(TF::GREEN."FlowerTitle has been enabled");
+   }
+   
+   public function onHand(DataPackReceiveEvent $event){
+   $player = $event->getPlayer();
+   $item = $player->getInventory()->getItemInHand()->getId();
+   
+   if($item == 37){
+   $player->sendTip(TF::GREEN . $player->getName() . TF::PURPLE . "ﻚﺑ ﻼﻫﺍ");
+  }
+ }
+}
